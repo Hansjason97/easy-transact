f81b0cbfd5a2836e47867c071ce6c18fa0af8ba6
Hansjason97 <hansadiogo@gmail.com>
2025-09-12 07:01:50 +0100

    configured the meta data of the website

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,4 +17,10 @@ export const navLinks:navLink[] = [
         title: "Contact",
         href: "contact"
     }
-] 
+];
+
+export const siteConfig = {
+    title: "Gpay",
+    description : "Gpay offre une API puissante pour intégrer les paiements digitaux (Mobile Payments, Virements, Agency Banking) dans vos applications. Découvrez notre écosystème complet de services financiers sécurisés conçu pour le marché camerounais. Rejoignez +50 000 utilisateurs et +100 institutions.",
+    defaultTitle : "Gpay - L'API de Paiement Digital pour la Fintech et le Cameroun | Services Bancaires & Mobile Money"
+}
